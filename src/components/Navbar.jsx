210171577ef263c0a53079a4bef947ce415fbcca
Karthik <karthikdevanath10@gmail.com>
2026-07-14 10:16:35 +0530

    branding: set notebook icon in both navbar and browser favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = ({ onOpenLogin }) => {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          <img src="/favicon.svg" alt="Academix Logo" style={{ height: '24px', width: 'auto' }} />
+          <Notebook size={24} className="brand-icon" style={{ color: 'var(--accent-color)' }} />
           <span>Academix</span>
         </Link>
 
